5008aa6dc983a0ba43f8a61e657fe617c7b0afe3
L <breu.lorenz@gmail.com>
2013-11-17 10:42:59 -0500

    fooarg

--- admin.php
+++ admin.php
@@ -1,9 +1,13 @@
 <html>
 <body>
 <?php
- echo anchor('main/showShowtimes','Show Showtimes') . "<br />";
- echo anchor('main/populate','Populate Database') . "<br />";
- echo anchor('main/delete','Delete Database') . "<br />";  
+
+<a href="uofttheater/index.php/main/showShowtimes">Show Showtimes</a><br>
+
+<a href="uofttheater/index.php/main/populate">Populate Database</a><br>
+
+<a href="uofttheater/index.php/main/delete">Delete Database</a>
+
 ?>
 </body>
 </html> 
