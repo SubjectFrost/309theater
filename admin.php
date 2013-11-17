635b35274f145c44cc8baded5f65ec9aa6fe4adf
L <breu.lorenz@gmail.com>
2013-11-17 13:33:53 -0500

    sdf

--- admin.php
+++ admin.php
@@ -10,10 +10,10 @@
 
 <a href="uofttheater/index.php/main/populate">Populate Database</a><br>
 
-<a href="uofttheater/index.php/main/delete">Delete Database (Keep Tickets)</a>
+<a href="uofttheater/index.php/main/delete">Delete Database (Keep Tickets)</a><br>
 
-<a href="uofttheater/index.php/main/fullDelete">Delete Full Database (Delete Tickets)</a>
+<a href="uofttheater/index.php/main/fullDelete">Delete Full Database (Delete Tickets)</a><br>
 
-<a href="uofttheater/index.php/main/delete_tickets">Delete Tickets</a>
+<a href="uofttheater/index.php/main/delete_tickets">Delete Tickets</a><br>
 </body>
 </html> 
