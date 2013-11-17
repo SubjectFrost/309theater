49fe6319142307891fc4520495486c305406b4ba
squeakysammi <squeakysammi@gmail.com>
2013-11-17 08:32:24 -0500

    stuff

--- result.php
+++ result.php
@@ -2,11 +2,42 @@
 <body>
 
 <?php
-$query = $this->db->query("select m.title, t.name, t.address, s.date, s.time, s.available
+	function get_showtimes()
+	{
+		$query = $this->db->query("select m.title, t.name, t.address, s.date, s.time, s.available
 								from movie m, theater t, showtime s
 								where m.id = s.movie_id and t.id=s.theater_id");
-		echo $query;
+		return $query;	
+	}  
 
+		
+		
+		
+		$this->load->library('table');
+		
+		
+		//Then we call our model's get_showtimes function
+		$showtimes = $this->get_showtimes();
+
+		//If it returns some results we continue
+		if ($showtimes->num_rows() > 0){
+		
+			//Prepare the array that will contain the data
+			$table = array();	
+	
+			$table[] = array('Movie','Theater','Address','Date','Time','Available');
+		
+		   foreach ($showtimes->result() as $row){
+				$table[] = array($row->title,$row->name,$row->address,$row->date,$row->time,$row->available);
+		   }
+			//Next step is to place our created array into a new array variable, one that we are sending to the view.
+			$data['showtimes'] = $table; 		   
+		}
+		
+		//Now we are prepared to call the view, passing all the necessary variables inside the $data array
+		$data['main']='main/showtimes';
+		$this->load->view('template', $data);
+    }
 ?>
 
 
