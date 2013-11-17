dd47217bcb056d999ad06ee14e9b8f06f8d4541a
squeakysammi <squeakysammi@gmail.com>
2013-11-17 08:29:42 -0500

    stuff

--- result.php
+++ result.php
@@ -2,31 +2,10 @@
 <body>
 
 <?php
-$this->load->library('table');
-		$this->load->model('showtime_model');
-		
-		
-		$showtimes = $this->showtime_model->get_showtimes();
-
-		
-		if ($showtimes->num_rows() > 0){
-		
-			
-			$table = array();	
-	
-			$table[] = array('Movie','Theater','Address','Date','Time','Available');
-		
-		   foreach ($showtimes->result() as $row){
-				$table[] = array($row->title,$row->name,$row->address,$row->date,$row->time,$row->available);
-		   }
-			
-			$data['showtimes'] = $table; 		   
-		}
-		
-		
-		$data['main']='main/showtimes';
-		$this->load->view('template', $data);
-
+$query = $this->db->query("select m.title, t.name, t.address, s.date, s.time, s.available
+								from movie m, theater t, showtime s
+								where m.id = s.movie_id and t.id=s.theater_id");
+		echo $query;
 
 ?>
 
