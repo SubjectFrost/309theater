474b2067ed53f182a2dc00246a99ee3234732417
L <breu.lorenz@gmail.com>
2013-11-17 03:08:29 -0500

    sgfd

--- phpversion.php
+++ phpversion.php
@@ -43,13 +43,13 @@ if (($_POST['seat3'] == "clicked") && ($_POST['seat1'] != "clicked") && ($_POST[
 
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat2\" value=\"not_clicked\" /><input type=\"image\" src=\"uofttheater/images/green.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat3\" value=\"not_clicked\" /><input type=\"image\" src=\"uofttheater/images/green.png\" name=\"submit\"/></form>";
 }
 else
 {
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat2\" value=\"clicked\" /><input type=\"image\" src=\"uofttheater/images/white.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat3\" value=\"clicked\" /><input type=\"image\" src=\"uofttheater/images/white.png\" name=\"submit\"/></form>";
 }
 ?>
 
