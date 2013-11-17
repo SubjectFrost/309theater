059718b9536813af6370f71d20f023fe992c9413
squeakysammi <squeakysammi@gmail.com>
2013-11-16 22:16:41 -0500

    blahg

--- index.php
+++ index.php
@@ -13,20 +13,19 @@
  
  
  
- <form action="<?php echo $_SERVER['PHP_SELF']; ?>"  onsubmit="return validateForm()" method="post">
- <p>Your name: <input type="text" name="name" id="name"; ?>"></p>
+ <form action="<?php echo $_SERVER["PHP_SELF"]; ?>"  onsubmit="return validateForm()" method="post">
+ <p>Your name: <input type="text" name="name" id="name"; ?></p>
  <p>Your age: <input type="text" name="age" id = "age"></p>
  <p><input type="submit" value="submit"></p>
-</form>
 
 <script type="text/javascript">
- var a = new LiveValidation('name');
+ var a = new LiveValidation("name");
  a.add(Validate.Presence);
- var b = new LiveValidation('age;);
+ var b = new LiveValidation("age");
  b.add(Validate.Numericality );
  </script>
-
-Hi <?php echo htmlspecialchars($_POST['name']); ?>.
-You are <?php echo (int)$_POST['age']; ?> years old.
+</form>
+Hi <?php echo htmlspecialchars($_POST["name"]); ?>.
+You are <?php echo (int)$_POST["age"]; ?> years old.
  </body>
 </html>
