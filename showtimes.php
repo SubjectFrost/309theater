64682d174538bf74867cba8520ea3dc32ef8d1fa
squeakysammi <squeakysammi@gmail.com>
2013-11-17 10:34:46 -0500

    sdf

--- showtimes.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo anchor('','Back') . "<br />";
-
-
-//And if the $site variable is not empty we echo it's content by using the generate method of the table class / library
-if(!empty($showtimes)) echo $this->table->generate($showtimes); 
-
-?>
-
