160f74886670b59111566a40da72facf7ba2978d
squeakysammi <squeakysammi@gmail.com>
2013-11-17 14:58:33 -0500

    Blah

--- index.php
+++ index.php
@@ -28,48 +28,8 @@ buttonImageOnly: true
  
  
  </form>
- 
- <?php
- class Main extends CI_Controller {
+ <a href = "uofttheater/index.php/main/showMovies">Show movies</a>
 
-    
-    function __construct() {
-    	// Call the Controller constructor
-    	parent::__construct();
-    }
-        
-    function index() {
-	    	$data['main']='uofttheater/main/index';
-	    	$this->load->view('template', $data);
-    }
-    
- //First we load the library and the model
-		$this->load->library('table');
-		$this->load->model('uofttheater/movie_model');
-		
-		//Then we call our model's get_showtimes function
-		$movies = $this->movie_model->get_movies();
-
-		//If it returns some results we continue
-		if ($movies->num_rows() > 0){
-		
-			//Prepare the array that will contain the data
-			$table = array();	
-	
-			$table[] = array('Movie id', 'Title');
-		
-		   foreach ($movies->result() as $row){
-				$table[] = array($row->id,$row->title);
-		   }
-			//Next step is to place our created array into a new array variable, one that we are sending to the view.
-			$data['movies'] = $table; 		   
-		}
-		
-		//Now we are prepared to call the view, passing all the necessary variables inside the $data array
-		$data['main']='uofttheater/main/movies';
-		$this->load->view('template', $data);
- }
- ?>
  </body>
  </html>
 
