44f5af1dbb5a686ab7388f02f83bbcdd555f93a1
L <breu.lorenz@gmail.com>
2013-11-16 22:53:49 -0500

    added pngs and stuffs

--- phpversion.php
+++ phpversion.php
@@ -11,31 +11,19 @@
 
 
 <div id = "buttons">
-<span style="float.right"><a href="#" onclick = "setSelection()" ><img
-name="cursor" src="cursor.png" width="30" height="30" border="1"
+<span style="float.right"><a href="#" onclick = "isSelected()" ><img
+name="cursor" src="green.png" width="30" height="30" border="1"
 alt="cursor image" title="selection"></a></span>
-<span style="float.right"><a href="#" onclick = "setIDLine()" ><img
-name="linebutton" src="line.png" width="30" height="30" border="1"
+
+<span style="float.right"><a href="#" onclick = "select()" ><img
+name="linebutton" src="white.png" width="30" height="30" border="1"
 alt="line image" title="draw a line"></a></span>
 
-<span style="float.right"><a href="#" onclick = "setIDBox()" ><img
-name="boxbutton" src="box.png" width="30" height="30" border="1"
+<span style="float.right"><a href="#" onclick = "taken()" ><img
+name="boxbutton" src="yellow.png" width="30" height="30" border="1"
 alt="square image" title="draw a square"></a></span>
 
-<span style="float.right"><a href="#" onclick = "setIDTri()" ><img
-name="tributton" src="tri.png" width="30" height="30" border="1"
-alt="triangle button" title="draw a triangle"></a></span>
 
-<span style="float.right"><a href="#" onclick = "Erase()" ><img
-name="erasebutton" src="eraser.png" width="30" height="30" border="1"
-alt="eraser button" title="erase shape"></a></span>
-
-<span style="float.right"><a href="#" onclick = "clearCanvas()" ><img
-name="clearbutton" src="clear.png" width="30" height="30" border="1"
-alt="clear button" title="remove all shapes"></a></span>
-<span style="float.right"><a href="#" onclick = "copyShape()" ><img
-name="copy" src="copy.png" width="30" height="30" border="1"
-alt="copy image" title="Copy and Paste"></a></span>
 
 
 </div>
---
+++ phpversion.php~
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>Test</title>
+<body>
+
+<?php echo '<p>Moo</p>';
+
+
+
+?>
+
+
+<div id = "buttons">
+<span style="float.right"><a href="#" onclick = "setSelection()" ><img
+name="cursor" src="cursor.png" width="30" height="30" border="1"
+alt="cursor image" title="selection"></a></span>
+<span style="float.right"><a href="#" onclick = "setIDLine()" ><img
+name="linebutton" src="line.png" width="30" height="30" border="1"
+alt="line image" title="draw a line"></a></span>
+
+<span style="float.right"><a href="#" onclick = "setIDBox()" ><img
+name="boxbutton" src="box.png" width="30" height="30" border="1"
+alt="square image" title="draw a square"></a></span>
+
+<span style="float.right"><a href="#" onclick = "setIDTri()" ><img
+name="tributton" src="tri.png" width="30" height="30" border="1"
+alt="triangle button" title="draw a triangle"></a></span>
+
+<span style="float.right"><a href="#" onclick = "Erase()" ><img
+name="erasebutton" src="eraser.png" width="30" height="30" border="1"
+alt="eraser button" title="erase shape"></a></span>
+
+<span style="float.right"><a href="#" onclick = "clearCanvas()" ><img
+name="clearbutton" src="clear.png" width="30" height="30" border="1"
+alt="clear button" title="remove all shapes"></a></span>
+<span style="float.right"><a href="#" onclick = "copyShape()" ><img
+name="copy" src="copy.png" width="30" height="30" border="1"
+alt="copy image" title="Copy and Paste"></a></span>
+
+
+</div>
+
+</body>
+</html>
