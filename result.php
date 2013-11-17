8a813305e39ed95b908df997e7f2dba1dc0a2987
squeakysammi <squeakysammi@gmail.com>
2013-11-17 08:18:32 -0500

    stuff

--- result.php
+++ result.php
@@ -2,13 +2,30 @@
 <body>
 
 <?php
+$this->load->library('table');
+		$this->load->model('showtime_model');
+		
+		
+		$showtimes = $this->showtime_model->get_showtimes();
 
-$query = $this->db->get('g1ight');
-
-foreach ($query->result() as $row)
-{
-    echo $row->ticket;
-}
+		
+		if ($showtimes->num_rows() > 0){
+		
+			
+			$table = array();	
+	
+			$table[] = array('Movie','Theater','Address','Date','Time','Available');
+		
+		   foreach ($showtimes->result() as $row){
+				$table[] = array($row->title,$row->name,$row->address,$row->date,$row->time,$row->available);
+		   }
+			
+			$data['showtimes'] = $table; 		   
+		}
+		
+		
+		$data['main']='main/showtimes';
+		$this->load->view('template', $data);
 
 ?>
 
