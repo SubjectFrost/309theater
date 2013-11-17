867e8b8ab7e332e02217d4e40512a162bcddbca9
squeakysammi <squeakysammi@gmail.com>
2013-11-16 23:43:51 -0500

    a;sldkfja

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-<script type="text/javascript" src="livevalidation.js"></script>
+
  <head>
   <title>Index</title>
  </head>
@@ -11,17 +11,17 @@
  <p>Expiration date: mm <input type = "text" name="exp_month" id = "exp_month" size = "2"> yy <input type="text" name = "exp_year" id = "exp_year" size = "2"></p>
  <p><input type="submit" value="submit"></p>
 
-<script type="text/javascript">
- var name = new LiveValidation("name");
- name.add(Validate.Presence);
- var card = new LiveValidation("card");
- card.add(Validate.Numericality, {onlyInteger: true} );
- var expm = new LiveValidation("exp_month");
- expm.add(Validate.Numericality, { minimum: 1, maximum: 12, onlyInteger: true };
- expm.add(Validate.Length, { is: 2 } ); 
-  var expy = new LiveValidation("exp_year");
- expy.add(Validate.Numericality, { minimum: 1, maximum: 99, onlyInteger: true };
- expy.add(Validate.Length, { is: 2 } );
+<script type="text/javascript"src="livevalidation.js">
+	var name = new LiveValidation("name");
+	name.add(Validate.Presence);
+	var card = new LiveValidation("card");
+	card.add(Validate.Numericality, {onlyInteger: true} );
+	var expm = new LiveValidation("exp_month");
+	expm.add(Validate.Numericality, { minimum: 1, maximum: 12, onlyInteger: true };
+	expm.add(Validate.Length, { is: 2 } ); 
+	var expy = new LiveValidation("exp_year");
+	expy.add(Validate.Numericality, { minimum: 1, maximum: 99, onlyInteger: true };
+	expy.add(Validate.Length, { is: 2 } );
  </script>
 </form>
 Hi <?php echo htmlspecialchars($_POST["name"]); ?>.
