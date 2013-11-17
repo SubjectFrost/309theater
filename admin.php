632fda325ba3dc4178a3cc6d6e219447be92980c
L <breu.lorenz@gmail.com>
2013-11-17 10:45:27 -0500

    sdf

--- admin.php
+++ admin.php
@@ -2,12 +2,12 @@
 <body>
 <?php
 
+?>
+
 <a href="uofttheater/index.php/main/showShowtimes">Show Showtimes</a><br>
 
 <a href="uofttheater/index.php/main/populate">Populate Database</a><br>
 
 <a href="uofttheater/index.php/main/delete">Delete Database</a>
-
-?>
 </body>
 </html> 
