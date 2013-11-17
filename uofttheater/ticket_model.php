679c436ac384886ed3df2e79836a6d2e28fa32a6
L <breu.lorenz@gmail.com>
2013-11-17 15:19:25 -0500

    asd

--- uofttheater/ticket_model.php
+++ uofttheater/ticket_model.php
@@ -3,15 +3,25 @@ class Ticket_model extends CI_Model {
 
 	function get_tickets()
 	{
+	//return all ticket information
 		$query = $this->db->query("select * from ticket");
 		return $query;	
 	}  
 
-	function populate() {
-		$this->db->query("insert into ticket (first,last,creditcardnumber,creditcardexpiration,showtime_id,seat) values ('foo','bar',1234567890123456,2015,5713,1)");
+	function add_ticket(first,last,creditcardnumber,creditcardexpiration,showtime_id,seat) {
+		//make sure there is an available seat
+		$available = $this->db->query("select available from showtime where id="+ showtime_id);
+		if ($available > 0){
+		      //insert ticket into the ticket table
+		      $this->db->query("insert into ticket (first,last,creditcardnumber,creditcardexpiration,showtime_id,seat) values ("first","last","creditcardnumber","creditcardexpiration","seat")");
+		      $available = $available - 1;
+		      $this->db->query("update showtime set available="+ $available +" where id="+ showtime_id);
+		}
+		
 	}
 
 	function delete() {
+	//delete all tickets
 		$this->db->query("delete from ticket");
 	}
 	
