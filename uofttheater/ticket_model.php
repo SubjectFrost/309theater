0925e2aca744ece356457d241da05fddced14fde
L <breu.lorenz@gmail.com>
2013-11-17 15:43:49 -0500

    asd

--- uofttheater/ticket_model.php
+++ uofttheater/ticket_model.php
@@ -10,14 +10,14 @@ class Ticket_model extends CI_Model {
 
 	function add_ticket(first,last,creditcardnumber,creditcardexpiration,showtime_id,seat) {
 		//make sure there is an available seat
-		$sqel = "select available from showtime where id=?";
-		$available = $this->db->query($sqel,array(showtime_id);
+		$sqel = "select available from showtime where id= ?";
+		$available = $this->db->query($sqel,array(showtime_id));
 		if ($available > 0){
 		      //insert ticket into the ticket table
-		      $sql = "insert into ticket (first,last,creditcardnumber,creditcardexpiration,showtime_id,seat) values (?,?,?,?,?,?)";
+		      $sql = "insert into ticket (first,last,creditcardnumber,creditcardexpiration,showtime_id,seat) values ( ? , ? , ? , ? , ? , ? )";
 		      $this->db->query($sql,array(first,last,creditcardnumber,creditcardexpiration,showtime_id,seat));
 		      $av = $available - 1;
-		      $sqll = "update showtime set available=? where id=?";
+		      $sqll = "update showtime set available= ? where id= ?";
 		      $this->db->query($sqll,array($av,showtime_id));
 		}
 		
