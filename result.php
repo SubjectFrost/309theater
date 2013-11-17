ccbd729e19f80ebc6dd00db1cfd6597a65decfe3
squeakysammi <squeakysammi@gmail.com>
2013-11-17 07:19:13 -0500

    stuff

--- result.php
+++ result.php
@@ -2,22 +2,16 @@
 <body>
 
 <?php
-$con=mysqli_connect();
 
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+$this->db->query("SELECT * FROM showtime");
 
-$result = mysqli_query($con,"SELECT * FROM showtime");
-
-while($row = mysqli_fetch_array($result))
+while($row = $this->db->query($result);)
   {
   echo $row['movie_id'] . " " . $row['theater_id'];
   echo "<br>";
   }
 
-mysqli_close($con);
+
 ?>
 
 
