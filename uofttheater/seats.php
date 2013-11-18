663861aea8bd0339ec03182af9f796947b262657
L <breu.lorenz@gmail.com>
2013-11-18 06:26:29 -0500

    Sdf

--- uofttheater/seats.php
+++ uofttheater/seats.php
@@ -9,20 +9,20 @@ if (($_POST['seat1'] == "clicked") && ($_POST['seat2'] != "clicked") && ($_POST[
 
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat1\" value=\"not_clicked\" /><input type=\"image\" src=\"../../images/green.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat1\" value=\"not_clicked\" /><input type=\"image\" src=\"../images/green.png\" name=\"submit\"/></form>";
 }
 else
 {
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat1\" value=\"clicked\" /><input type=\"image\" src=\"../../images/white.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat1\" value=\"clicked\" /><input type=\"image\" src=\"../images/white.png\" name=\"submit\"/></form>";
 }
 }
 else
 {
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"image\" src=\"../../images/yellow.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"image\" src=\"../images/yellow.png\" name=\"submit\"/></form>";
 }
 
 
@@ -31,20 +31,20 @@ if (($_POST['seat2'] == "clicked") && ($_POST['seat1'] != "clicked") && ($_POST[
 
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat2\" value=\"not_clicked\" /><input type=\"image\" src=\"../../images/green.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat2\" value=\"not_clicked\" /><input type=\"image\" src=\"../images/green.png\" name=\"submit\"/></form>";
 }
 else
 {
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat2\" value=\"clicked\" /><input type=\"image\" src=\"../../images/white.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat2\" value=\"clicked\" /><input type=\"image\" src=\"../images/white.png\" name=\"submit\"/></form>";
 }
 }
 else
 {
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"image\" src=\"../../images/yellow.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"image\" src=\"../images/yellow.png\" name=\"submit\"/></form>";
 }
 
 
@@ -53,20 +53,20 @@ if (($_POST['seat3'] == "clicked") && ($_POST['seat1'] != "clicked") && ($_POST[
 
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat3\" value=\"not_clicked\" /><input type=\"image\" src=\"../../images/green.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat3\" value=\"not_clicked\" /><input type=\"image\" src=\"../images/green.png\" name=\"submit\"/></form>";
 }
 else
 {
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat3\" value=\"clicked\" /><input type=\"image\" src=\"../../images/white.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"hidden\" name=\"seat3\" value=\"clicked\" /><input type=\"image\" src=\"../images/white.png\" name=\"submit\"/></form>";
 }
 }
 else
 {
 echo "<form action=\"";
 echo $_SERVER["PHP_SELF"];
-echo "\" method=\"POST\"><input type=\"image\" src=\"../../images/yellow.png\" name=\"submit\"/></form>";
+echo "\" method=\"POST\"><input type=\"image\" src=\"../images/yellow.png\" name=\"submit\"/></form>";
 }
 
 }
