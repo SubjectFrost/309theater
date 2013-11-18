ee2b547032bf150bce0580d3861885768f311dea
squeakysammi <squeakysammi@gmail.com>
2013-11-18 00:23:53 -0500

    blah

--- uofttheater/result_model.php
+++ uofttheater/result_model.php
@@ -5,7 +5,7 @@ class result_model extends CI_Model {
 	{
 		$query = $this->db->query("select m.title, t.name, t.address, s.date, s.time, s.available
 								from movie m, theater t, showtime s
-								where m.id = $movieID , t.id= $theaterID , s.date = $date");
+								where m.id = $movieID , t.id = $theaterID , s.date = $date");
 		return $query;	
 	}  
 
