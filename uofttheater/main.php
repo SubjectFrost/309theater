189f2b20f5ece6c06ee7cb73d572311ad90c9656
squeakysammi <squeakysammi@gmail.com>
2013-11-18 00:45:07 -0500

    blah

--- uofttheater/main.php
+++ uofttheater/main.php
@@ -48,19 +48,20 @@ class Main extends CI_Controller {
 	
 	function showResults()
     {
-		echo $_POST["movieID"];
+		/*echo $_POST["movieID"];
 		echo $_POST["theaterID"];
 		echo $_POST["date"];
 		if ($_POST["movieID"] == "") {
 		echo "lulz";
 		}
+		*/
 		//First we load the library and the model
 		$this->load->library('table');
 		//$this->load->model('result_model');
 		
 		//Then we call our model's get_showtimes function
 		//$results = $this->result_model->get_results(&_POST["movieID"], $_POST["theaterID"], $_POST["date"]);
-		$results = null;
+		//$results = null;
 		
 		//$this->db->select('*');
 		//$this->db->from('movie m , theater t, showtime s');
@@ -70,38 +71,38 @@ class Main extends CI_Controller {
 		//if ($_POST["$theaterID"] != '0') $this->db->where('t.id',$_POST["$theaterID"]);
 		//if ($_POST["$date"] != '0') $this->db->where('s.time', $_POST["$date"]);
 		//$results = $this->db->get();
-		/*
-		if ($_POST["movieID"] != null && $_POST["theaterID"] == null && $_POST["date"] == null) {
+		
+		if ($_POST["movieID"] != "" && $_POST["theaterID"] == "" && $_POST["date"] == "") {
 		$sql = "select m.title, t.name, t.address, s.date, s.time, s.available	from movie m, theater t, showtime s where m.id = ?";
 		
 		$results = $this->db->query($sql,array($_POST["movieID"]));
 		
 		}
-		if ($_POST["movieID"] == null && $_POST["theaterID"] != null && $_POST["date"] == null) {
+		if ($_POST["movieID"] == "" && $_POST["theaterID"] != "" && $_POST["date"] == "") {
 		$sql = "select m.title, t.name, t.address, s.date, s.time, s.available	from movie m, theater t, showtime s where t.id = ?";
 		$results = $this->db->query($sql,array($_POST["theaterID"]));
 		
 		}
-		if ($_POST["movieID"] == null && $_POST["theaterID"] == null && $_POST["date"] != null) {
+		if ($_POST["movieID"] == "" && $_POST["theaterID"] == "" && $_POST["date"] != "") {
 		$sql = "select m.title, t.name, t.address, s.date, s.time, s.available	from movie m, theater t, showtime s where s.date = ?"
 		$results = $this->db->query($sql,array($_POST["date"]));
 		
 		}
-		if ($_POST["movieID"] != null && $_POST["theaterID"] != null && $_POST["date"] == null) {
+		if ($_POST["movieID"] != "" && $_POST["theaterID"] != "" && $_POST["date"] == "") {
 		$sql = "select m.title, t.name, t.address, s.date, s.time, s.available	from movie m, theater t, showtime s where m.id = ? and t.id = ?";
 		$results = $this->db->query($sql,array($_POST["movieID"], $_POST["theaterID"]));
 		}
 		
 		
-		if ($_POST["movieID"] != null && $_POST["theaterID"] == null && $_POST["date"] != null) {
+		if ($_POST["movieID"] != "" && $_POST["theaterID"] == "" && $_POST["date"] != "") {
 		$sql = "select m.title, t.name, t.address, s.date, s.time, s.available	from movie m, theater t, showtime s where m.id = ? and s.date = ?";
 		$results = $this->db->query($sql,array($_POST["movieID"], $_POST["date"]));
 		}
-		if ($_POST["movieID"] == null && $_POST["theaterID"] != null && $_POST["date"] != null) {
+		if ($_POST["movieID"] == "" && $_POST["theaterID"] != "" && $_POST["date"] != "") {
 		$sql = "select m.title, t.name, t.address, s.date, s.time, s.available	from movie m, theater t, showtime s where t.id = ? and s.date = ?";
 		$results = $this->db->query($sql,array($_POST["theaterID"], $_POST["date"]));
 		}
-		*/
+		
 		
 		//If it returns some results we continue
 		if ($results->num_rows() > 0){
