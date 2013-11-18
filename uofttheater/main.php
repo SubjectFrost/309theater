dea837fdefbd5b8b107426dbad292a7a11300aa0
squeakysammi <squeakysammi@gmail.com>
2013-11-18 03:05:11 -0500

    blah

--- uofttheater/main.php
+++ uofttheater/main.php
@@ -251,7 +251,7 @@ class Main extends CI_Controller {
 	  
 	  if (($ccn_digits == 16) && ($ccex_digits == 4)) {
 		  $this->load->model('ticket_model');
-		  return $this->ticket_model->add_ticket($_POST["fname"],$_POST["lname"],$_POST["ccn"],$_POST["ccex"],11729,1);
+		  return $this->ticket_model->add_ticket($_POST["fname"],$_POST["lname"],$_POST["ccn"],$_POST["ccex"],$_POST["showtime_id"],1);
 	  }
 	  return -1;
 	  
